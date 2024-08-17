04f41eb283a3651fbd08c91a45296a0e98e6e62f
Poonam-Srinivas <poonamsrinivasthakur@gmail.com>
2024-08-18 00:32:08 +0530

    final changes

--- screens/Call.js
+++ screens/Call.js
@@ -1,9 +1,9 @@
-import {StyleSheet, View, Button, Text} from 'react-native';
-import React, {useState} from 'react';
 import {
-  ZegoUIKitPrebuiltCall,
   GROUP_VIDEO_CALL_CONFIG,
+  ZegoUIKitPrebuiltCall,
 } from '@zegocloud/zego-uikit-prebuilt-call-rn';
+import React, {useEffect, useState} from 'react';
+import {StyleSheet, View} from 'react-native';
 
 export default function CallPage({route, navigation}) {
   const {name, id} = route.params;
@@ -17,37 +17,37 @@ export default function CallPage({route, navigation}) {
     setCallIDs(callIDs.filter(id => id !== callID));
   };
 
+  useEffect(() => {
+    handleJoinCall(id);
+  }, []);
+
   return (
     <View style={styles.container}>
-      {callIDs.map((callID, index) => (
-        <ZegoUIKitPrebuiltCall
-          key={index}
-          appID={82066232}
-          appSign={
-            '9d93cf5d29dc9e92b64ef646af825344dd588151a4636f0fd1f784a8707b07c1'
-          }
-          userID={name}
-          userName={name}
-          callID={callID}
-          config={{
-            ...GROUP_VIDEO_CALL_CONFIG,
-            onHangUp: () => {
-              handleLeaveCall(callID);
-              if (callIDs.length === 1) {
-                navigation.navigate('Home');
-              }
-            },
-          }}
-        />
-      ))}
-      <Button
-        title="Join New Call"
-        onPress={() => handleJoinCall('newCallID')}
-      />
+      {callIDs.length > 0 &&
+        callIDs.map((callID, index) => (
+          <ZegoUIKitPrebuiltCall
+            key={index}
+            appID={82066232}
+            appSign={
+              '9d93cf5d29dc9e92b64ef646af825344dd588151a4636f0fd1f784a8707b07c1'
+            }
+            userID={name}
+            userName={name}
+            callID={callID}
+            config={{
+              ...GROUP_VIDEO_CALL_CONFIG,
+              onHangUp: () => {
+                handleLeaveCall(callID);
+                if (callIDs.length === 1) {
+                  navigation.navigate('Home');
+                }
+              },
+            }}
+          />
+        ))}
     </View>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
